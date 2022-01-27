3d3f32f5377300dd72b857e8968d380728aaa63e
Stefan Rom <srom@samhammer.de>
2022-01-27 13:18:06 +0100

    #12 Backport to dotnet 3.1

--- src/Samhammer.DependencyInjection.Override.Test/TestData/FactoryClass/OverrideClassFromFactory.cs
+++ src/Samhammer.DependencyInjection.Override.Test/TestData/FactoryClass/OverrideClassFromFactory.cs
@@ -1,5 +1,6 @@
-namespace Samhammer.DependencyInjection.Override.Test.TestData.FactoryClass;
-
-public class OverrideClassFromFactory : ParentClassFromFactory
+namespace Samhammer.DependencyInjection.Override.Test.TestData.FactoryClass
 {
+    public class OverrideClassFromFactory : ParentClassFromFactory
+    {
+    }
 }
--- src/Samhammer.DependencyInjection.Override.Test/TestData/InjectedAllClass/InjectAllParentClass.cs
+++ src/Samhammer.DependencyInjection.Override.Test/TestData/InjectedAllClass/InjectAllParentClass.cs
@@ -1,16 +1,17 @@
 using Samhammer.DependencyInjection.Attributes;
 
-namespace Samhammer.DependencyInjection.Override.Test.TestData.InjectedAllClass;
-
-[Inject(Target.All)]
-public class InjectAllParentClass : IClassMultiple1, IClassMultiple2
+namespace Samhammer.DependencyInjection.Override.Test.TestData.InjectedAllClass
 {
-}
+    [Inject(Target.All)]
+    public class InjectAllParentClass : IClassMultiple1, IClassMultiple2
+    {
+    }
 
-public interface IClassMultiple1
-{
-}
+    public interface IClassMultiple1
+    {
+    }
 
-public interface IClassMultiple2
-{
+    public interface IClassMultiple2
+    {
+    }
 }
